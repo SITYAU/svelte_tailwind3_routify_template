e36cae01ac65302d9aa06b9d9051e5c2a968c7f0
sityau <phil@sity.com.au>
2022-03-29 17:18:40 +1100

    tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./client/**/*.{html,js,svelte,ts}",
+    "./imports/ui/**/*.{html,js,svelte,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
